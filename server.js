0ceb7338a83195b73556c2c449d44fbcab4d1fd1
prabhjot <psingh20112002@gmail.com>
2023-07-03 01:49:31 +0530

    debug

--- server.js
+++ server.js
@@ -39,6 +39,11 @@ const corsOption = {
 
     
 };
+app.use(function(req, res, next) {  
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});  
 
 app.use(cors(corsOption));
 app.use('/storage', express.static('storage'));
